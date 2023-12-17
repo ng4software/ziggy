5db2fadba727505cb1325db8bbda004f35b65589
NG4 Software <ngfour@protonmail.com>
2023-12-17 02:31:22 +0100

    add todo

--- modules/ziggy.cmp.zig
+++ modules/ziggy.cmp.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 // Basic string comparison, is case sensitive.
 // TODO: Add args to allow for a) trimming and b) case insensitive comparison.
+// TODO: Does this not need to live in a "str" module?
 pub inline fn strEq(first: []const u8, second: []const u8) bool {
     return std.mem.eql(u8, first, second);
 }
